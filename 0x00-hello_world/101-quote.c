888082daf7477f7c648de06b6953a780e19f187f
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-13 12:59:50 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  * Return: Always 0
  */
 
-int main(void)
+int main(char)
 {
-	printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
