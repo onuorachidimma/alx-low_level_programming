5a7a8d76af7c5af7adc202cd288ef511a2a3be45
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-13 12:18:49 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
