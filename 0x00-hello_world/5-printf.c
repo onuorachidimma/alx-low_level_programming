abd888be14e3536a657f92cb1d38ad6e4bf828ac
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-13 06:20:55 -0700

    Puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints with proper grammar, but the outcome is a piece of art
+ *
+ * return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
