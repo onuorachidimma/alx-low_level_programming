d1700c625571e62143559e5a17f4f05b00bf36c2
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-13 06:26:33 -0700

    Puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints with proper grammar, but the outcome is a piece of art
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
  *
  * return: Always 0
  */
