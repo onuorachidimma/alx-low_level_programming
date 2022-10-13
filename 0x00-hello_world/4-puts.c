98f82fef407ff128cbc12a00c18ef7d26dcd6f30
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-13 05:23:23 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point, prints "Programming is like building a multilingual puzzle
  * Return 0 (Successful)
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
