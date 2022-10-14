37b1d09b1e7f3923199e21574cd55028924e61ff
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-14 13:03:34 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+	char c;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	return (0);
+}
