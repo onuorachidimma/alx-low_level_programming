7128efe2dd237332a34d6af210b0afdf2266ec80
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-14 13:31:15 -0700

    When I was having that alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return - Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' && ch == 'q')
+	{
+		continue;
+	}
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}	
