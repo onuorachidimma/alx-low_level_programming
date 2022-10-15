4f9895cabe506106838f53e65f4ff8ac6824328b
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-14 20:08:18 -0700

    When I was having that alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - "Prints The alphabet in lowercase followed by a new line."
+ * main - Prints the alphabet in lowercase, must be followed by a new line
  *
  * Return - Always 0
  */
