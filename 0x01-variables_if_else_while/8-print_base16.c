110c28807109840e9a9589020b3e33fa17fa92f8
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-14 18:57:32 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int base16;
 
-	for (base16 = '0'; bas16 <= '9'; base16++)
+	for (base16 = '0'; base16 <= '9'; base16++)
 		putchar(base16);
 	for (base16 = 'a'; base16 <='f'; base16++)
 		putchar(base16);
