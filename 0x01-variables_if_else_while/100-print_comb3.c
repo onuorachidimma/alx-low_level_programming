ccc4a02535f329bb0ee0e1ba6ce2951b6c7b6dda
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-16 11:29:06 -0700

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,21 @@ int main(void)
 {
 	int num;
 
-	for (num = '01'; num <= '89'; num++)
+	for (num = "01"; num <= "89"; num++)
 	{
 		putchar(num);
-		if(num != '89')
+		if(num != "89")
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		if(num != 10-11 && num != 30-33 && num !=  40-44)
+		if(num != "10"-"11" && num != "30"-"33" && num != "40-44")
 			putchar(num);
 		{
-		if(num != 60-66 && num != 70-78 && num != 80-88)
+		if(num != "60"-"66" && num != "70"-"78" && num != "80"-"88")
 			putchar(num);
 		}
+	}
 	putchar('\n');
 	return (0);
 }
