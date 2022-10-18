bd31120c1b9064994855e9eb253055ac8fd082e1
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-18 05:24:31 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,18 @@
  * Return 0
  */
 
+void _putchar(char a)
+
 int main(void)
 {
-	putchar(_);
-	putchar(p);
-	putchar(u);
-	putchar(t);
-	putchar(c);
-	putchar(h);
-	putchar(a);
-	putchar(r);
-	putchar('\n');
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar('\n');
 	return (0);
 }
