594489c0e0863ab2e6febb815f8e9298fdf46830
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-18 12:01:26 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#includ "main.h"
+
+/**
+ * main - Prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(char alpha, char bet)
+{
+	char ch = char alpha <= char; alpa++;
+	_putchar('ch');
+	_putchar('\n');
+}
+int main(void)
+{
+	print_alphabet(a, z);
+	return (0);
+}
