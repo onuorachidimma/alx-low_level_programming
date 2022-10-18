d04cdb4e2fa448efbc05a901dd3baf9c2874442f
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-18 06:53:25 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,7 @@
  */
 
 int main(void)
+int _putchar(char c)
 {
 	_putchar('_');                                                                                       
 	_putchar('p');                                                                                       
