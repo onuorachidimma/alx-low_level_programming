9a52ac358a20f5b0a411f39cf07a7a1d65c2ae34
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-20 16:24:01 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle, using the character #.
+ * @size: The size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+	int draw, tri;
+
+	if (size > 0)
+	{
+		for (draw = 1; draw <= size; draw++)
+		{
+			for (tri = size - draw; tri > 0; tri--)
+				_putchar(' ');
+
+			for (tri = 0; tri < draw; tri++)
+				_putchar('#');
+
+			if (hash == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
