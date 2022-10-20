73df2a38a3a0a071fdd3fb7bc7982bccced3886b
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-20 14:45:10 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Prints the numbers from 1-100, but for multiples of three,
- * 	  Fizz is printed instead of the number, for multiples of five,
- * 	  Buzz, and for multiples of both three and five, FizzBuzz.
+ * Fizz is printed instead of the number, for multiples of five,
+ * Buzz, and for multiples of both three and five, FizzBuzz.
  *
  * Return: Always 0.
  */
