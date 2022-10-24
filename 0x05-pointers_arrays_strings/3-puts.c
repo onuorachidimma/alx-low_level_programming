6a7a7a814bdad85c66a17259242b14a11ff34066
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-24 04:59:41 -0700

    I do not fear computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * void _puts - This function prints a string, followed by a new line
- * @:str: Pointer to the string
- * Return: Reuturns the value of the string 
+ * _puts - This function prints a string, followed by a new line
+ * @str: Pointer to the string
+ * Return: Reuturns the value of the string
  */
 
 void _puts(char *str)
