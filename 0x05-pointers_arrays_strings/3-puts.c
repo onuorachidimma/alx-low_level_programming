f36b9daa8d39f690e46e88045517c05abc9da4f6
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-24 04:30:29 -0700

    I do not fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * void _puts - This function prints a string, followed by a new line
+ * @:str: Pointer to the string
+ * Return: Reuturns the value of the string 
+ */
+
+void _puts(char *str)
+{
+	_puts(str\n);
+}
