383c0170f33f4a76717b7d6b7c621a61b1984d02
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-24 05:09:48 -0700

    I can only go one way

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_rev - This function prints a string, in reverse, followed by a new line.
+ * @s: Pointer to the string
+ * Return: Returns the string 
+ */
+
+void print_rev(char *s);
+{
+	while (*str)
+		_putchar(*str--);
+	_putchar('\n');
+}
