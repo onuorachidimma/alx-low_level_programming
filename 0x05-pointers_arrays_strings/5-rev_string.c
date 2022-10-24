641fe0f6527f025ebf70a80e19010d7d54cde01a
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-24 13:36:07 -0700

    A good engineer thinks in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,15 +10,18 @@ void rev_string(char *s)
 {
 	int rev;
 	char str;
+	int rev1;
 
 	rev = 0;
+	rev1= 0;
 	while (s[rev])
 		rev++;
 
-	while (0 > rev)
+	while (rev1 < rev)
 		rev--;
 	{
-		s[rev--] = s[rev];
+		str = s[rev1];
+		s[rev1++] = s[rev];
 		s[rev] = str;
 	}
 }
