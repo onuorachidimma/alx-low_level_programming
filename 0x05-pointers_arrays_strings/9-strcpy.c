6e712425df571bd5eb3f12a9b703dbf21276f90a
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-24 18:56:51 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,6 +5,7 @@
  * including the terminating null byte (\0), to the buffer pointed to by dest
  * @dest: A pointer to destination of string
  * @src: A pointer to source string to copy from
+ * Return: pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
