49cbe55f15e46abb09b9c2f6106cb4ea4759314a
Chidimma Onuora <onourachidimma4@gmail.com>
2022-10-27 12:58:04 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,25 +5,18 @@
  * @dest: an input string
  * @src: an input string
  * @n: an input integer
- * @start: an input string
  * Return: A pointer to the resulting string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+	int index = 0, src_len = 0;
 
-	while (*src)
-	{
-		srclen++;
-		src++;
-	}
-	srclen++;
-	if (n > srclen)
-		n = srclen;
-	src = start;
-	for (; i < n; i++)
-		*dest++ = *src++;
-	return (temp);
+	while (src[index++])
+		src_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+	return (dest);
 }
