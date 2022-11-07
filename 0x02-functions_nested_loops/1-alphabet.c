02edd368499ab12b54b7e77c51aa478a33662d04
Chidimma Onuora <onourachidimma4@gmail.com>
2022-11-07 11:38:01 -0800

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet, in lowercase, followed by a new line.
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Always 0
  */
 
 void print_alphabet(void)
 {
 	char c;
-	
+
 	for (c = 'a'; 'a' <= 'z'; c++)
 	{
 		_putchar('c');
