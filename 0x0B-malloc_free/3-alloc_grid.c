4ea8e5d18eea7d53bf66df73f601b89eb8947ee8
Chidimma Onuora <onourachidimma4@gmail.com>
2022-11-14 00:40:46 -0800

    If you even dream of beating me you would better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,6 +15,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+
 	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
 		return (NULL);
