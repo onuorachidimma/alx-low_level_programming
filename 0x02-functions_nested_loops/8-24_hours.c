03a2e03e7664694de2988d56b54e55cefd91666a
Chidimma Onuora <onourachidimma4@gmail.com>
2022-12-22 05:33:38 -0800

    Updated

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print every minute of the day
+ *
+ * Return: Void
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(':');
+			_putchar((minute / 10) + 48);
+			_putchar((minute % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
