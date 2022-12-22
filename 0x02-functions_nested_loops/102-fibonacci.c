8673b77c98bdce4b01bfcae965c0be85e2388fe4
Chidimma Onuora <onourachidimma4@gmail.com>
2022-12-22 06:57:35 -0800

    Updated

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+/**
+ * main - entry point
+ * Fibonacci sequence
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int64_t num1 = 1;
+	int64_t num2 = 2;
+	int64_t nextnum = num1 + num2;
+
+	printf("%" PRId64 ", ", num1);
+	printf("%" PRId64 ", ", num2);
+	printf("%" PRId64 ", ", nextnum);
+
+	for (i = 4; i <= 50; i++)
+	{
+		num1 = num2;
+		num2 = nextnum;
+		nextnum = num1 + num2;
+
+		if (i < 50)
+		{
+			printf("%" PRId64 ", ", nextnum);
+		}
+		else
+		{
+			printf("%" PRId64 "\n", nextnum);
+		}
+	}
+	return (0);
+}
